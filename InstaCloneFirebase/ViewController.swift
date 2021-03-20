169acd1c13e21e9eb8cf1058effc636eb1c56bf2
Tolga Kağan <tolga.kagan@hotmail.com>
2021-03-20 17:19:49 +0300

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Tolga Kağan Aysu on 20.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
